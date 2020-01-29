ad769b73845eed48dbbd2a76322639e6d3295296
financeiro03 <financeiro03@FIN03.rsatacadao.local>
2020-01-29 14:39:59 -0300

    criação do frame incluir pessoa

---
+++ CadCrud/src/br/com/main/view/IncluirPessoaFrame.java
@@ -0,0 +1,129 @@
+package br.com.main.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class IncluirPessoaFrame extends JFrame {
+
+	private JTextField tfNome;
+	private JTextField tfEndereco;
+	private JTextField tfCidade;
+	private JTextField tfEstado;
+	private JTextField tfTelefone;
+	private JTextField tfEmail;
+	private JTextField tfCpf;
+	private JTextField tfRg;
+	private JFormattedTextField tfDataNascimento;
+	
+	private JTextField tfDescricao;
+	private JTextField tfPreco;
+	private JFormattedTextField tfId;
+	
+	private JButton bSalvar;
+	private JButton bCancelar;
+	protected JButton bExcluir;
+	
+	private ListaPessoasFrame framePrincipal;
+	
+	
+	public IncluirPessoaFrame(ListaPessoasFrame framePrincipal) {
+		this.framePrincipal = framePrincipal;
+		setTitle("Incluir Pessoa");
+		setSize(300,250);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		inicializaComponentes();
+		resetForm();
+	}
+	
+	private void inicializaComponentes() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(montaPanelEditarPessoa(), BorderLayout.CENTER);
+		panel.add(montaPanelBotoesEditar(), BorderLayout.SOUTH);
+		add(panel);
+		
+		GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+	}
+	private JPanel montaPanelBotoesEditar() {
+		JPanel panel = new JPanel();
+
+//		bSalvar = new JButton("Salvar");
+//		bSalvar.setMnemonic(KeyEvent.VK_S);
+//		bSalvar.addActionListener(new SalvarPessoaListener());
+//		panel.add(bSalvar);
+//
+//		bCancelar = new JButton("Cancelar");
+//		bCancelar.setMnemonic(KeyEvent.VK_C);
+//		bCancelar.addActionListener(new CancelarListener());
+//		panel.add(bCancelar);
+//		
+//		bExcluir = new JButton();
+//		bExcluir.setText("Excluir");
+//		bExcluir.setMnemonic(KeyEvent.VK_E);
+//		bExcluir.addActionListener(new ExcluirPessoaListener());
+//		bExcluir.setVisible(false);
+//		panel.add(bExcluir);
+
+		return panel;
+	}
+	private JPanel montaPanelEditarPessoa() {
+		JPanel painelEditarMercadoria = new JPanel();
+		painelEditarMercadoria.setLayout(new GridLayout(8, 1));
+		
+		tfNome = new JTextField();
+		tfDescricao = new JTextField();
+		tfPreco = new JTextField();
+		tfQuantidade = new JFormattedTextField();
+		tfId = new JFormattedTextField();
+		tfId.setEnabled(false);
+
+		painelEditarMercadoria.add(new JLabel("Nome:"));
+		painelEditarMercadoria.add(tfNome);
+		painelEditarMercadoria.add(new JLabel("Descrição:"));
+		painelEditarMercadoria.add(tfDescricao);
+		painelEditarMercadoria.add(new JLabel("Preço:"));
+		painelEditarMercadoria.add(tfPreco);
+		painelEditarMercadoria.add(new JLabel("Quantidade:"));
+		painelEditarMercadoria.add(tfQuantidade);
+		painelEditarMercadoria.add(new JLabel("Id:"));
+		painelEditarMercadoria.add(tfId);
+		
+		return painelEditarMercadoria;
+	}
+	private void resetForm() {
+		tfId.setValue(null);
+		tfNome.setText("");
+		tfDescricao.setText("");
+		tfPreco.setText("");
+		tfQuantidade.setValue(new Integer(1));
+	}
+	
+	
+}
